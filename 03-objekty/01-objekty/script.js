0b6e0cbf990746883f2a5ac5f70c7cf99957841a
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-03-12 21:51:00 +0100

    objekty

--- 03-objekty/01-objekty/script.js
+++ 03-objekty/01-objekty/script.js
@@ -11,17 +11,21 @@ const svobodna = true;
 
 
 //  jedna proměnná, která obsahuje objekt a ten má vlastnosti
-// object literal - definice objektu
+// object literal - uvnitř není blok ale definice objektu - jmeno = key, Alena = value
 const osoba = {
 	jmeno: 'Alena',
 	prijmeni: 'Nováková',
 	rokNarozeni: 1996,
 	email: 'alena.novakova@neznam.cz',
-	adresa: {
+	adresa: {// hodnota vlastnosti je další objekt
 		ulice: 'Dvorská',
 		cislo: 6,
 		mesto: 'Městečko nad Řekou',
 		psc: '199 37',
+		gps:{
+			lon: '199',
+			lat: 185,
+		}
 	},
 	svobodna: true,
 };
@@ -29,26 +33,31 @@ const osoba = {
 
 // tečková notace
 osoba.jmeno = 'Jana';
-console.log( osoba.jmeno );
 console.log(osoba.jmeno);
+console.log(osoba.adresa);
 console.log(osoba.prijmeni);
 console.log(osoba.email);
 console.log(osoba);
 
 
-// závorková notace
+/* závorková notace - jedna výhoda oproti tečkové: klíč je zde jako textový řetězec a mohu ho mít uložené 
+v proměnné . Závorková notece umožňuje přistupovat k vlastnostem jejichž jméno mám v jiné proměnné.Nevím dopředu,
+která vlastnost to bude. */
 osoba['rokNarozeni'] = 2001;
+osoba['prijmeni']
 console.log( osoba['rokNarozeni'] );
+console.log( osoba['prijmeni'] );
 
 
 
-// závorková notace umožňuje mít
+// závorková otace umožňuje mít
 // název vlastnosti uložený v proměnné
 const vlastnost = 'jmeno';
 console.log( osoba[vlastnost] );
 
 
 // vnořené objekty
+console.log(osoba.adresa.ulice);
 console.log( osoba.adresa.mesto );
 console.log( osoba['adresa']['mesto'] );
 
@@ -58,6 +67,9 @@ console.log( osoba['adresa']['mesto'] );
 // když na objektu neexistuje, tak se přidá
 
 osoba.vzdelani = 'VŠ';
+osoba.vek = 50;
+osoba.svobodna = false;
+console.log(osoba);
 
 
 // smazání vlastnosti objektu
